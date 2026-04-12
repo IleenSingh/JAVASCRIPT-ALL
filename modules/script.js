eda1a5c093e28f0c0d160f396095d17b0546b6ca
ileen <ileensingh14@gmail.com>
2026-04-12 22:48:57 +0530

    commit

---
+++ modules/script.js
@@ -0,0 +1,13 @@
+//let - jab value aage jaake badalni hai 
+//const - value change ni hoti kbhi
+//var(es5 ka part hai) -container stores values
+
+//var
+var username = "ileen";
+//window meain add hota hai 
+//function scoped hota hai
+// phir se declare krrskte hai but error ni ata hai
+
+//let
+//let use krna h hmesha kyuki ye unidentified error se bachata hai and 
+ 
