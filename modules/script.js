27de5fc510643f1254019ca91cf6420fac41a797
ileen <ileensingh14@gmail.com>
2026-04-13 00:16:15 +0530

    commit

--- modules/script.js
+++ modules/script.js
@@ -9,5 +9,25 @@ var username = "ileen";
 // phir se declare krrskte hai but error ni ata hai
 
 //let
+let a =100;
 //let use krna h hmesha kyuki ye unidentified error se bachata hai and 
+//redeclaration is not used with let
  
+
+//const -no change
+const nickname = "frooti";
+
+//Scope [global , block , functional]
+// Block Scope → Code inside {} like in loops, if , etc.
+// Function Scope → Code inside a function
+// let and const follow block scope.
+// var ignores block scope — which leads to bugs.
+
+
+
+                //  Temporal Dead zone
+// The Temporal Dead Zone is the time between:
+// when a variable is declared and when it is initialized (assigned a value)
+// During this period, you cannot access the variable, even though it is in scope.
+console.log(answer);
+let answer = "yes";
